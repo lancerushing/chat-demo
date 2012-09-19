c98c51ecd0e35b05d0d2f8f79dd70030c2a61223
Lance Rushing <lance@lancerushing.com>
2012-09-19 18:04:10 +0000

    Adding index handler

--- router.php
+++ router.php
@@ -2,12 +2,17 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-if (isset($_SERVER['PATH_INFO'] )) {
-	$test_file_name = __DIR__ . '/routes' . $_SERVER['PATH_INFO'] . '.php';
+if (isset($_SERVER['REQUEST_URI'] )) {
+	if ($_SERVER['REQUEST_URI'] === "/") {
+		$_SERVER['REQUEST_URI'] = "/index";
+	}
+	$test_file_name = __DIR__ . '/routes' . $_SERVER['REQUEST_URI'] . '.php';
 	if(is_file($test_file_name)) {
 		require_once $test_file_name;
 		return ;
 	}
 
 }
-return FALSE;
+
+header("HTTP/1.0 404 Not Found");
+echo "<h1>Not Found</h1>";
